4a119e5205dedc7d94c0cc69e9d148382bc305a3
Manoj Bojja <manojbojja@Manojs-MacBook-Pro.local>
2018-08-10 01:57:20 +0530

    internnet and json checks

--- FPTECH2/Constants.swift
+++ FPTECH2/Constants.swift
@@ -9,8 +9,6 @@
 import Foundation
 
 let BASE_URL = "https://jasonbase.com/things/zKy1"
-//let BASE_URL = "https://live.shawacademy.com/api/v1/leads/5af4adf1b5ba4eba4d8b4883"
-
 let ERROR_NO_INTERNET = "No Internet Connection"
 let ERROR_JSON_PARSE = "JSON format error"
 typealias DownloadComplete = () -> ()
